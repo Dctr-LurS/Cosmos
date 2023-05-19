6081fcaf48a72ef49482a1a1682aac293ff1d3d7
Adrian Rodriguez Garcia <124038232+Dctr-LurS@users.noreply.github.com>
2023-05-19 10:25:21 +0200

    Cosmos - V2

--- Cosmos/JavaScript/main.js
+++ Cosmos/JavaScript/main.js
@@ -1,6 +1,22 @@
 const video = document.querySelector('.video-background');
 let contenidoV = false;
 let mContenido = document.getElementById("cu-oculto"); 
+let carousel = document.querySelector('.conector-carrusel');
+let carouselItems = document.querySelectorAll('.imagen-carrusel');
+let currentIndex = 0;
+let totalItems = carouselItems.length;
+
+function showItem(index) {
+  if (index >= 0 && index < totalItems) {
+    carousel.style.transform = 'translateX(-' + (index * 100) + '%)';
+    currentIndex = index;
+  }
+}
+
+setInterval(function() {
+  var nextIndex = (currentIndex + 1) % totalItems;
+  showItem(nextIndex);
+}, 3000);
 
 function mostrarContenido(){
     if(contenidoV){
